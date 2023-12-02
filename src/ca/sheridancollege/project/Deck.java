06f2a070ad3bba3f26c26b4b4ba1f2d8462b9f85
Sumeetpal Singh <sin12684@sheridancollege.ca>
2023-12-02 18:07:26 -0500

    Third Commit For Deliverable 3 (Complete Code)

--- src/ca/sheridancollege/project/Deck.java
+++ src/ca/sheridancollege/project/Deck.java
@@ -7,15 +7,24 @@ package ca.sheridancollege.project;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
+/**
+ * Represents a deck of cards used in the game of Blackjack.
+ */
 public class Deck {
 
+    // The list of cards in the deck
     private ArrayList<BlackjackCard> cards;
 
+    /**
+     * Constructs a new deck by initializing it with standard playing cards.
+     */
     public Deck() {
         this.cards = new ArrayList<>();
+        // Define standard suits and ranks for a deck of cards
         String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+        // Create cards for each combination of suit and rank
         for (String suit : suits) {
             for (String rank : ranks) {
                 cards.add(new BlackjackCard(rank, suit));
@@ -23,12 +32,20 @@ public class Deck {
         }
     }
 
+    /**
+     * Shuffles the cards in the deck.
+     */
     public void shuffle() {
         Collections.shuffle(cards);
     }
 
+    /**
+     * Draws a card from the deck.
+     *
+     * @return the card drawn from the deck.
+     */
     public BlackjackCard draw() {
+        // Remove and return the first card in the deck
         return cards.remove(0);
     }
-
 }
--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -8,41 +8,77 @@ package ca.sheridancollege.project;
 import java.util.ArrayList;
 
 /**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
+ * The base class that models a card game. Students should create a more specific child class and implement the given
+ * methods.
+ *
+ * This class provides a basic structure for managing players in a game.
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
 public abstract class Game {
 
+    // The name of the game
     private String name;
+
+    // The list of players in the game
     private ArrayList<Player> players;
 
+    /**
+     * Constructs a new game with a given name.
+     *
+     * @param name the name of the game.
+     */
     public Game(String name) {
         this.name = name;
         this.players = new ArrayList<>();
     }
 
+    /**
+     * Gets the name of the game.
+     *
+     * @return the name of the game.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the list of players in the game.
+     *
+     * @return the list of players.
+     */
     public ArrayList<Player> getPlayers() {
         return players;
     }
 
+    /**
+     * Adds a player to the game.
+     *
+     * @param player the player to add.
+     */
     public void addPlayer(Player player) {
         players.add(player);
     }
 
+    /**
+     * Removes a player from the game.
+     *
+     * @param player the player to remove.
+     */
     public void removePlayer(Player player) {
         players.remove(player);
     }
 
+    /**
+     * Clears the list of players in the game.
+     */
     public void clearPlayers() {
         players.clear();
     }
 
+    /**
+     * Abstract method to be implemented by specific game classes. It represents the main logic of playing the game.
+     */
     public abstract void play();
 }
